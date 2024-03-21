84b81a769d5f738e0ab81f915353b709b5172756
Amani Jammoul <amani.jammoult@mail.mcgill.ca>
2024-03-20 21:42:56 -0400

    static analysis - perfect score

--- server.py
+++ server.py
@@ -1,25 +1,46 @@
+"""
+This file initiates the Flask application of emotion detection to be 
+    executed over the Flask channel and deployed on localhost:5000.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detection
-
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    This function calls the emotion_detection function with the given
+        text as 'textToAnalyze'.
+
+    Returns:
+    str: text that provides emotion scores and dominant emotion
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     res = emotion_detection(text_to_analyze)
 
     if res['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
-    return "For the given statement, the system response is 'anger': {}, \
-            'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The \
-            dominant emotion is {}.".format(str(res['anger']), str(res['disgust']), \
-            str(res['fear']), str(res['joy']), str(res['sadness']), \
-            res['dominant_emotion'])
+    # return "For the given statement, the system response is 'anger': {}, \
+    #         'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The \
+    #         dominant emotion is {}.".format(str(res['anger']), str(res['disgust']), \
+    #         str(res['fear']), str(res['joy']), str(res['sadness']), \
+    #         res['dominant_emotion'])
+    output = (
+        f"For the given statement, the system response is 'anger': "
+        f"{str(res['anger'])}, 'disgust': {str(res['disgust'])}, 'fear': "
+        f"{str(res['fear'])}, 'joy': {str(res['joy'])} and 'sadness': "
+        f"{str(res['sadness'])}. The dominant emotion is {res['dominant_emotion']}."
+    )
+    return output
 
 @app.route("/")
 def render_index_page():
+    """
+    This function initiates the rendering of the main application page over
+        the Flask channel.
+    """
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
